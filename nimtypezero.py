d5a862f7380fa0f25d01b7e61c88a67742b17511
Cart-Monke <113125216+Cart-Monke@users.noreply.github.com>
2023-01-21 20:37:40 -0800

    Added Player class

--- nimtypezero.py
+++ nimtypezero.py
@@ -1,9 +1,54 @@
 import numpy as np
-np.random.seed(seed=1000)
+import random
+import datetime as dt
+
+# np.random.seed(430598)
+
+BRAIN_STRUCTURE = [90, 12, 12, 12, 18]
+BRAIN_LENGTH = len(BRAIN_STRUCTURE)
 
 
 def ReLU(x):
     return x if x > 0 else 0
+
+
+def sigmoid(x):
+    return 1 / 1 + np.exp(-x)
+
+
+def randomList(len):
+    l = []
+
+    for _ in range(len):
+        l.append(np.random.random())
+
+    return l
+
+# Mixes the values of any two arrays. Will also mix nested arrays if they're at the same index.
+# Ex: [[5, 10], 10] and [[20, 40], 3] might produce [[5, 40], 3] as an output
+
+
+def mixArr(mother, father):
+    child = []
+
+    minLength = min(len(mother), len(father))
+    difference = abs(len(mother) - len(father))
+
+    for i in range(minLength):
+        a = mother[i]
+        b = father[i]
+
+        if (isinstance(a, list) and isinstance(b, list)):
+            child.append(mixArr(a, b))
+        else:
+            child.append(random.choice([a, b]))
+
+    if difference > 0:
+        longerParent = mother if len(mother) > len(father) else father
+
+        child.extend(longerParent[minLength:])
+
+    return child
 
 
 class Network(object):
@@ -23,25 +68,28 @@ class Network(object):
 
         # Initialize weight and bias connections
         for i in range(numLayers - 1):
+            # current and next nodeCount
             cNodeCount = len(self.nodes[i])
             nNodeCount = len(self.nodes[i + 1])
 
             self.weights.append([[0]*nNodeCount]*cNodeCount)
             self.biases.append([[0]*nNodeCount]*cNodeCount)
 
-    @property
+    # Setters and getters
+    @ property
     def outputs(self):
         return self.nodes[len(self.nodes) - 1]
 
-    @property
+    @ property
     def inputs(self):
         return self.nodes[0]
 
-    @inputs.setter
+    @ inputs.setter
     def inputs(self, value):
         self.nodes[0] = value
 
-    def calculate(self):
+    # The function used to update the neural networks values
+    def propogate(self):
         for i, currentLayer in enumerate(self.nodes[1:]):
             layerWeights = self.weights[i]
             layerBiases = self.biases[i]
@@ -55,9 +103,35 @@ class Network(object):
 
                 currentLayer[j] = ReLU(sum)
 
+    # Extra functions for making random networks
     def randomizeConnections(self, connections):
         for layer in connections:
             layerSize = len(layer[0])
 
             for i in range(len(layer)):
-                layer[i] = np.random.random(layerSize)
+                layer[i] = randomList(layerSize)
+
+    def randomize(self):
+        self.randomizeConnections(self.weights)
+        self.randomizeConnections(self.biases)
+
+
+class Player(object):
+    def __init__(self, parents=None, shouldMutate=False):
+        self.network = Network(BRAIN_LENGTH, BRAIN_STRUCTURE)
+
+        if (parents):
+            self.network.weights = mixArr(
+                parents[0].weights, parents[1].weights)
+            self.network.biases = mixArr(parents[0].biases, parents[1].biases)
+        else:
+            self.network.randomize()
+
+    # Setters
+    @property
+    def weights(self):
+        return self.network.weights
+
+    @property
+    def biases(self):
+        return self.network.biases
